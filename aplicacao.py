bd4869dbe4e323a2a26533ffeb3b0d50bc0c5356
VictorHoffman20 <142947563+VictorHoffman20@users.noreply.github.com>
2024-06-19 21:28:24 -0300

    Add files via upload

---
+++ aplicacao.py
@@ -0,0 +1,10 @@
+# aplicacao.py
+
+def main():
+    nome = "Victor Augusto Malaquias Hoffman"
+    matricula = "198462"
+    print(f"Nome: {nome}")
+    print(f"Matrícula: {matricula}")
+
+if __name__ == "__main__":
+    main()
