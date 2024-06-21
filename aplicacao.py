76d2e97b0fd7d549e863774f3e45231ae99967ea
VictorHoffman20 <142947563+VictorHoffman20@users.noreply.github.com>
2024-06-20 22:28:31 -0300

    Update aplicacao.py
    Atualizado para executar uma contagem.

--- aplicacao.py
+++ aplicacao.py
@@ -1,10 +1,14 @@
-# aplicacao.py
-
 def main():
-    nome = "Victor Augusto Malaquias Hoffman"
-    matricula = "198462"
-    print(f"Nome: {nome}")
-    print(f"Matrícula: {matricula}")
+    nome = "Victor Augusto Malaquias Hoffman"  
+    matricula = "198462"  
+
+    numero = int(input("Digite um número até o qual você deseja contar: "))
+
+    for i in range(1, numero + 1):
+        print(i)
+
+    print(f"\nNome: {nome}, Matrícula: {matricula}")
 
 if __name__ == "__main__":
     main()
+
